078566ca1880720d8304be0daa83bb63ab6a9857
bloodycrayfish <m1992steen@gmail.com>
2024-08-17 15:27:27 +0200

    Added some things

--- helpers/handler.go
+++ helpers/handler.go
@@ -2,7 +2,8 @@ package helpers
 
 import (
 	"errors"
-	"go-do-the-thing/app/shared"
+	"go-do-the-thing/app/shared/models"
+	"log"
 	"net/http"
 )
 
@@ -15,6 +16,28 @@ func AcceptHeaderSwitch(w http.ResponseWriter, r *http.Request, jsonFunc func(w 
 		w.Header().Set("Content-Type", "text/html")
 		uiFunc(w, r)
 	} else {
-		shared.HttpError("No Content-type specified", errors.New("no content-type specified in request"), w)
+		HttpError("No Content-type specified", errors.New("no content-type specified in request"), w)
 	}
+}
+
+type ErrorPage struct {
+	ActiveScreens models.NavBarObject
+	Message       string
+	ErrorMessage  string
+}
+
+func newErrorPage(message string, err error) ErrorPage {
+	activeScreens := models.NewNavbarObject()
+	activeScreens.IsError = true
+	return ErrorPage{activeScreens, message,
+		err.Error()}
+}
+
+func HttpErrorUI(templates Templates, message string, err error, w http.ResponseWriter) {
+	errorPage := newErrorPage(message, err)
+	err = templates.RenderWithCode(w, http.StatusInternalServerError, "error", errorPage)
+}
+func HttpError(message string, err error, w http.ResponseWriter) {
+	log.Println(message, err)
+	http.Error(w, message, http.StatusInternalServerError)
 }
