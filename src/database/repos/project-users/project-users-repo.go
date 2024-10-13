42bfd211c6e77c97ed5a0fc941b4aba7b72b703d
crimsoncrayfish <m1992steen@gmail.com>
2024-10-13 08:09:10 +0200

    some more procs

--- src/database/repos/project-users/project-users-repo.go
+++ src/database/repos/project-users/project-users-repo.go
@@ -11,7 +11,7 @@ type ProjectUsersRepo struct {
 	database database.DatabaseConnection
 }
 
-// NOTE: Depends on: [./users-repo.go, ./projects-repo.go]
+// NOTE: Depends on: [./users-repo.go, ./projects-repo.go, ../roles/roles-repo.go]
 func InitRepo(database database.DatabaseConnection) *ProjectUsersRepo {
 	logger := slog.NewLogger("project users repo")
 	_, err := database.Exec(createProjectUsersTable)
@@ -24,9 +24,19 @@ func InitRepo(database database.DatabaseConnection) *ProjectUsersRepo {
 }
 
 const (
-	createProjectUsersTable = `CREATE TABLE IF NOT EXISTS tags (
-	[id] INTEGER PRIMARY KEY,
-   	[name] TEXT DEFAULT '' NOT NULL,
+	createProjectUsersTable = `CREATE TABLE IF NOT EXISTS project_users (
+	[project_id] INTEGER,
+	[user_id] INTEGER,
+	[role_id] INTEGER,
+	FOREIGN KEY (project_id) REFERENCES projects(id)
+	FOREIGN KEY (user_id) REFERENCES users(id),
+	FOREIGN KEY (role_id) REFERENCES roles(id)
 );`
-	getAllProjectUsers = `SOME SQL HERE TO GET ALL TAGS`
+	getAllForProject = `SELECT [project_id], [user_id], [role_id] FROM project_users WHERE project_id = ?`
+	getAllForUser    = `SELECT [project_id], [user_id], [role_id] FROM project_users WHERE user_id = ?`
+	insert           = `INSERT INTO project_users (project_id, user_id, role_id) VALUES (?, ?, ?)`
+	update           = `UPDATE project_users SET [role_id] = ? WHERE [project_id] = ? AND [user_id] = ?`
+	delete           = `DELETE FROM project_users WHERE [project_id] = ? AND [user_id] = ?`
+	//assertUserIsInProject = ``
+	//assertUserHasRole     = ``
 )
