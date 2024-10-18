0fc75fc4cf842daf9904b70a7e8c0cf2e8c7584b
crimsoncrayfish <m1992steen@gmail.com>
2024-10-18 07:01:33 +0200

    starting to build project pages

--- src/database/time.go
+++ src/database/time.go
@@ -32,6 +32,9 @@ func (t *SqLiteTime) Scan(v interface{}) error {
 }
 
 func (t *SqLiteTime) Value() (driver.Value, error) {
+	if t.Time == nil {
+		return 0, nil
+	}
 	return t.Time.Unix(), nil
 }
 
