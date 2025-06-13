c744b86d6fd6847bbeabbebf635a1eb241e80c02
Marco Steenkamp <m1992steen@gmail.com>
2025-06-13 15:40:15 +0200

    cleanup

--- src/database/database.go
+++ src/database/database.go
@@ -2,9 +2,7 @@ package database
 
 import (
 	"database/sql"
-	"fmt"
 	"go-do-the-thing/src/helpers/assert"
-	"strconv"
 
 	_ "github.com/mattn/go-sqlite3"
 )
@@ -37,46 +35,3 @@ func (db DatabaseConnection) Exec(query string, args ...any) (sql.Result, error)
 	assert.NotNil(db.Connection, source, "the db connection should not be nil")
 	return db.Connection.Exec(query, args...)
 }
-
-func (db DatabaseConnection) DoesColumnExistOnTable(table, column string) (bool, error) {
-	assert.NotNil(db.Connection, source, "the db connection should not be nil")
-	queryString := fmt.Sprintf(checkIfColumnExists, table, column)
-	query, err := db.Connection.Prepare(queryString)
-	if err != nil {
-		return false, err
-	}
-	defer query.Close()
-
-	var countString string
-	err = query.QueryRow().Scan(&countString)
-	if err != nil {
-		return false, err
-	}
-	count, err := strconv.Atoi(countString)
-	if err != nil {
-		return false, err
-	}
-	return count > 0, nil
-}
-
-func (db DatabaseConnection) AddColumnToTable(tableName, columnName, columnType string) error {
-	assert.NotNil(db.Connection, source, "the db connection should not be nil")
-	update := fmt.Sprintf(migrationAddColumn, tableName, columnName, columnType)
-
-	exists, err := db.DoesColumnExistOnTable(tableName, columnName)
-	if err != nil {
-		return err
-	}
-	if exists {
-		return nil
-	}
-	_, err = db.Connection.Exec(update)
-
-	return err
-}
-
-const (
-	migrationAddColumn  = `ALTER TABLE %s ADD %s %s`
-	migrationDropColumn = `ALTER TABLE %s DROP %s`
-	checkIfColumnExists = `SELECT COUNT(*) AS CNTREC FROM pragma_table_info('%s') WHERE name='%s'`
-)
